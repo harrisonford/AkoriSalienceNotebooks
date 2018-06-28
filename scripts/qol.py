0dd87f0dc213782ecb6035f4adfc59511ad9acb4
Kristofher Muñoz Rojas <kristofher@ug.uchile.cl>
2018-06-28 02:02:16 -0400

    Adds progress bar.

--- scripts/qol.py
+++ scripts/qol.py
@@ -23,10 +23,69 @@ def make_heatmap(fixmap, sigma=30, **kwargs):
     heatmap = ft.gaussian_filter(fixmap.astype('float'), sigma=sigma)
 
     if normalize == 'sum':
-        return heatmap/np.sum(heatmap)
+        return heatmap / np.sum(heatmap)
     elif normalize == 'keep':
-        return heatmap*max_fix/np.max(heatmap)
+        return heatmap * max_fix / np.max(heatmap)
     elif normalize == 'norm':
-        return heatmap/np.max(heatmap)
+        return heatmap / np.max(heatmap)
     else:
         return heatmap
+
+
+# taken from https://github.com/alexanderkuk/log-progress
+# noinspection PyTypeChecker
+def log_progress(sequence, every=None, size=None, name='Items'):
+    from ipywidgets import IntProgress, HTML, VBox
+    from IPython.display import display
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(sequence)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)  # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, record in enumerate(sequence, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = '{name}: {index} / ?'.format(
+                        name=name,
+                        index=index
+                    )
+                else:
+                    progress.value = index
+                    label.value = u'{name}: {index} / {size}'.format(
+                        name=name,
+                        index=index,
+                        size=size
+                    )
+            yield record
+    except ValueError:  # TODO: except should be bare by PEP8 standards added ValueError for now
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = index
+        label.value = "{name}: {index}".format(
+            name=name,
+            index=str(index or '?')
+        )
